8e3f37e502fa91112f0777ddb8030f679e6e78b9
MickaelFarre <mickael.farre@outlook.fr>
2022-12-09 14:39:07 +0100

    Mike exercice fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -10,6 +10,19 @@ var fizzbuzz = function (x) {
 //
 // YOUR CODE GOES HERE
 //
+    if(x === 2) {
+        return 2;
+    } else if(x % 3 === 0 && x % 5 === 1) {
+        return "fizz";
+    } else if(x % 3 > 0 && x % 5 === 0) {
+        return "buzz";
+    } else if(x % 3 === 0 && x % 5 === 0) {
+        return "fizzbuzz";
+    } else if(x % 3 != 0 && x % 5 != 0) {
+        return x;
+    } 
 };
+
+fizzbuzz(2);
 
 module.exports = { fizzbuzz: fizzbuzz };
